e8f079c0291406ac3518da03057b063121e34cba
Isha Negi <itsishanegi@gmail.com>
2024-10-12 20:56:19 +0530

    Add files via upload

---
+++ searching-an-element-in-array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{ int arr[]={12,43,556,34,23,56,78,67,64,343,2323,234,432};
+  int n;
+    printf("Enter any positive integer number(1-13) whose element you want to find: ");
+    scanf("%d",&n);
+       if(n>=1&&n<=13){
+            printf("%d",arr[n-1]);
+            
+        }
+        else{
+            printf("you entered something invalid");
+            
+        }
+}
